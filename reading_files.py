32eba412211947b748aafceff1945be838aa5468
kNosek1618 <k.nosek@ievent-management.pl>
2019-06-12 12:00:45 +0200

    add polish characters

--- reading_files.py
+++ reading_files.py
@@ -1,5 +1,10 @@
 
-variable = open("files.txt", "r")
+#second information is 'mode'
+#'r' - only read information
+#'w' - can change information
+#'a' - can append new informaction but can't change older
+#'r+' - you have all the power in reading and writing
+variable = open("files.txt", "r+")
 
-print(variable)
+print(variable.read())
 variable.close()
